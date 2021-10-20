9e4b31c530f5a4eefb81cfc47b7dcfbd0ecf5fb0
tshirt519 <tshirt519@gmail.com>
2021-10-20 11:19:17 +0900

    詳細画面と投稿機能追加

--- app/forms.py
+++ app/forms.py
@@ -2,7 +2,7 @@ from django import forms
 
 
 class PostForm(forms.Form):
-  title = forms.CharField(max_length=30, Label='タイトル')
+  title = forms.CharField(max_length=30, label='タイトル')
   content = forms.CharField(label='内容', widget=forms.Textarea())
   
 
--- app/urls.py
+++ app/urls.py
@@ -3,4 +3,6 @@ from app import views
 
 urlpatterns = [
   path('', views.IndexView.as_view(), name='index'),
+  path('post/<int:pk>', views.PostDetailView.as_view(), name='post_detail'),
+  path('post/new', views.CreatePostView.as_view(), name='post_new'),
 ]
